ef2401273def8c529e5093e83e19f3048108c307
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-01-01 15:30:40 -0300

    ajustes na caixa de busca antes de implementar mecanismo de busca

--- src/components/CaixaBusca/index.js
+++ src/components/CaixaBusca/index.js
@@ -1,15 +1,25 @@
+import { useEffect, useState } from 'react'
 import './CaixaBusca.css'
 
 const CaixaDeBusca = () => {
+    const mostraInput = (evento) => {
+        const input = evento.target.value
+        const resultado = document.querySelector('#itemTeste')
+        input.length > 1 ?
+            resultado.innerHTML = `nenhum resultado encontrado para "${input}"`
+        : resultado.innerHTML = ""
+    }
+
+    const [resultados, setResultados] = useState([[]])
+
     return (
         <div className="caixa-busca">
             <div className="caixa-busca__inputs">
-                <input type="text" className="caixa-busca__input" placeholder="O que deseja encontrar?" id="inputBusca" />
+                <input type="text" className="caixa-busca__input" onChange={mostraInput} placeholder="O que deseja encontrar?" id="inputBusca" />
                 <span className="material-symbols-outlined caixa-busca__lupa">search</span>
             </div>
             <div className="caixa-busca__resultados" id="resultados">
-                <a className="caixa-busca__resultados__item">Caneca</a>
-                <a className="caixa-busca__resultados__item">Pedestal</a>
+                <a className="caixa-busca__resultados__item" id='itemTeste'></a>
             </div>
         </div>
     )
