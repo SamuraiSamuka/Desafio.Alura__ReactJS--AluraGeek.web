c388ee1856af992438342fd6c7f6632e32816da0
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-01-02 14:41:17 -0300

    efeitos de texto no banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -5,8 +5,8 @@ const Banner = (props) => {
     return (
         <div className='banner__container container' style={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.195) 40%, #0009 100%), url('./Images/banner-2.webp')"}}>
             <div className='banner'>
-                <h1 className='banner__titulo'>Dezembro Promocional</h1>
-                <h3 className='banner__sub-titulo'>Produtos selecionados com 33% de desconto</h3>
+                <h1 className='banner__titulo efeitos'>Dezembro Promocional</h1>
+                <h3 className='banner__sub-titulo efeitos'>Produtos selecionados com 33% de desconto</h3>
                 <Botao>Ver Diversos</Botao>
             </div>
         </div>
